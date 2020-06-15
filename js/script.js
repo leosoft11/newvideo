9c29d8a54c462dbaa09802339a5d983563672e7c
Антон Кузнецов <antonkuznecov@Antons-MacBook-Air.local>
2020-06-15 13:00:29 +0400

    add js script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+var name = 'hello'
+const firstname = 'min'
+let age = 26
 
+
+
+const yearsago = prompt('введите ваш год рождения')
+const yearsago1 = prompt('какой сейчас год?')
+yearsagotest = yearsago1 - yearsago
+
+alert('Вам' + ' ' +yearsagotest + '?' + 'Я угадал?')
