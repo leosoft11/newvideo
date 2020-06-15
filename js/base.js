8005b2cb79fd46be020bd8e495c61fc337038be7
leosoft <admin@kek.com>
2020-06-15 23:58:15 +0400

    add two js file

---
+++ js/base.js
@@ -0,0 +1,67 @@
+// var name = 'hello'
+// const firstname = 'min'
+// let age = 26
+//
+
+//
+// const name = prompt('введите имя')
+// const yearsago = prompt('введите ваш год рождения')
+// const yearsago1 = prompt('какой сейчас год?')
+// yearsagotest = yearsago1 - yearsago
+//
+// alert('Вам' + ' ' +yearsagotest + '?' + 'Я угадал?')
+//
+// // if (yearsago === 'admin' && yearsago1 === 'password'){
+// //     alert('Доброе пожаловать домой')
+// //
+// // }else {
+// //       alert('Прости')
+// // }
+// const admin = 'admin'
+// const password = 'password'
+
+// https://www.youtube.com/watch?v=Bluxbh9CaQ0&list=WL&index=73&t=20110s 1 час 28 минут
+
+// function calculate(yearsago) {
+//    return 2020 - yearsago
+// }
+//
+// function infolog(name, yearsago) {
+//    const age = calculate(yearsago)
+//   alert('человек по имени' + naantome + ' возраст ' + yearsago)
+// }
+//
+// infolog(yearsago, name)
+// infolog('vlad', 1992)
+//          Массивы
+// const cars = ['car', 'merc', 'ford']
+// const admin = 'car'
+// if (cars === 'car'){
+//   alert('массив существует')
+// }
+// else {
+//     alert('такого массива нету')
+// }
+
+// const cars = ['car', 'merc', 'ford']
+//
+// for (let car of cars) {
+//   console.log(car)
+// }
+
+// обьекты
+
+// const person = {
+//    Fname: 'Anton',
+//    Lname: 'Kuznetsov',
+//    year: 1997,
+//    language: ['ru', 'eng'],
+//    greet: function () {
+//       console.log('function')
+//    }
+// }
+// const key = 'language'
+// person.kek = true
+// console.log(person[key])
+// console.log(person)
+// person.greet()
---
+++ js/javascript.js
@@ -0,0 +1,16 @@
+// const num = 42
+// const float = 42.42
+// const pow = 5*100
+// console.log(num, float, pow);
+
+// const name = 'Антон'
+// const age = 22
+//
+// const output = `приветик меня зовут ${name} мне ${age < 30 } аххахаха`
+//
+// // условие
+// const outputs = `приветик меня зовут ${name} мне ${age < 30 ? 'Все верно' : 'вообще то тебе еще не больше 30'} аххахаха`
+// console.log(output)
+// console.log(outputs)
+
+// 2:44:29 / 5:47:26
