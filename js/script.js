391ae42c992f5b69ed321cb78f597dfbc53f5f32
leosoft <admin@kek.com>
2020-06-15 15:24:41 +0400

    edit js script

--- js/script.js
+++ js/script.js
@@ -1,11 +1,35 @@
-var name = 'hello'
-const firstname = 'min'
-let age = 26
+// var name = 'hello'
+// const firstname = 'min'
+// let age = 26
+//
 
+//
+ const name = prompt('введите имя')
+ const yearsago = prompt('введите ваш год рождения')
+ // const yearsago1 = prompt('какой сейчас год?')
+// yearsagotest = yearsago1 - yearsago
+//
+// alert('Вам' + ' ' +yearsagotest + '?' + 'Я угадал?')
+//
+// // if (yearsago === 'admin' && yearsago1 === 'password'){
+// //     alert('Доброе пожаловать домой')
+// //
+// // }else {
+// //       alert('Прости')
+// // }
+// const admin = 'admin'
+// const password = 'password'
 
+// https://www.youtube.com/watch?v=Bluxbh9CaQ0&list=WL&index=73&t=20110s 1 час 28 минут
 
-const yearsago = prompt('введите ваш год рождения')
-const yearsago1 = prompt('какой сейчас год?')
-yearsagotest = yearsago1 - yearsago
+function calculate(yearsago) {
+   return 2020 - yearsago
+}
 
-alert('Вам' + ' ' +yearsagotest + '?' + 'Я угадал?')
+function infolog(name, yearsago) {
+   const age = calculate(yearsago)
+  alert('человек по имени' + naantome + ' возраст ' + yearsago)
+}
+
+infolog(yearsago, name)
+infolog('vlad', 1992)
