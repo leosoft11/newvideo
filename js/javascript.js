cd8f17f53bac6bd9ea4351d388d0b7b8cfb5a5ec
leosoft <admin@kek.com>
2020-06-16 23:32:52 +0400

    edit html file and js

--- js/javascript.js
+++ js/javascript.js
@@ -14,3 +14,43 @@
 // console.log(outputs)
 
 // 2:44:29 / 5:47:26
+
+
+window.onscroll = function() {
+  myFunction()
+};
+
+function myFunction() {
+  if (document.documentElement.scrollTop > 500)  {
+    var newDiv = document.createElement('div')
+     newDiv.className = 'my-class'
+
+
+
+
+
+  }
+}
+
+// этот код меняет поведение прокрутки на "smooth"
+// document.querySelector('html').onclick = function keks()  {
+//     const al = prompt('Введи сюда кое что')
+//
+//     if (al === '') {
+//        alert('Эй ты че введи текст чувак')
+//        document.body.style.background = 'red'
+//        setTimeout(() => document.body.style.background = '', 15000)
+//       return keks()
+//     }
+//       const myheading = document.querySelector('.intro-text')
+//       myheading.textContent = al
+//
+//     }
+
+
+// const person = {
+//    al() {
+//      alert('ты че введи текст')
+//    }
+// }
+// person.al()
